621524542ffd536317c954b64c925dbeaa8afa79
yasirshahid <yasirshahid5@gmail.com>
2024-12-21 19:39:33 +0500

    add some lambda

---
+++ sam-output-sample/app.py
@@ -0,0 +1,11 @@
+import json
+
+def lambda_handler(event,context):
+    personId = event["queryStringParameters"]["personId"]
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "personId": personId + "from lambda"
+        })
+    }
